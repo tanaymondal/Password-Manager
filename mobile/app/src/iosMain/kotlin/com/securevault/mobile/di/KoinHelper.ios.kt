6a8a993edc7b2dda542853e240c8b888f5432486
Tanay Mondal <tanaymondal6@gmail.com>
2026-05-31 19:02:35 +0530

    fix: add remember to iOS koinInject to prevent ViewModel recreation on recomposition

--- mobile/app/src/iosMain/kotlin/com/securevault/mobile/di/KoinHelper.ios.kt
+++ mobile/app/src/iosMain/kotlin/com/securevault/mobile/di/KoinHelper.ios.kt
@@ -1,11 +1,14 @@
 package com.securevault.mobile.di
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import org.koin.core.Koin
 
 var _koin: Koin? = null
 
 @Composable
 actual inline fun <reified T : Any> koinInject(): T {
-    return _koin!!.get()
+    return remember {
+        _koin!!.get<T>()
+    }
 }
